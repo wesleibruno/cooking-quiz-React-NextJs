cccac7c718bc380506dc9744988a1ff7b8ce6767
Weslei Bruno <weslei945@gmail.com>
2024-04-17 15:51:12 -0300

    Finished

---
+++ src/components/Results.tsx
@@ -0,0 +1,23 @@
+import { Question } from "@/types/Question";
+
+type Props = {
+  questions: Question[];
+  answers: number[];
+};
+export default function Results({ questions, answers }: Props) {
+  return (
+    <div>
+      {questions.map((item, key) => (
+        <div key={key} className="mb-3">
+          <div className="font-bold">
+            {key + 1}. {item.question}
+          </div>
+          <div>
+            <span>{item.answer === answers[key] ? "✅" : "❌"} - </span>
+            {item.options[item.answer]}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/data/questions.ts
@@ -0,0 +1,14 @@
+import { Question } from "@/types/Question";
+
+export const questions: Question[] = [
+  {
+    question: "Qual ingrediente não pode faltar?",
+    options: ["Farinha", "Corante", "Água", "Açúcar"],
+    answer: 1,
+  },
+  {
+    question: "Qual melhor método de cozinhar?",
+    options: ["Microondas", "Fogão", "Fogueira", "Lança-chamas"],
+    answer: 3,
+  },
+];
